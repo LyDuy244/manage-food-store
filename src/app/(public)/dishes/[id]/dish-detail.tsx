4e0a9b0c3cbb1ab2ce7408a6d4da25a86d5be4b1
Lý Nguyễn Ngọc Duy <118716140+LyDuy244@users.noreply.github.com>
2024-09-28 17:34:04 +0700

    Create feature Preview Modal Dish Detail and Dish Detail Page with parallel route and intercepting route

---
+++ src/app/(public)/dishes/[id]/dish-detail.tsx
@@ -0,0 +1,34 @@
+import dishApiRequest from "@/apiRequests/dish";
+import { formatCurrency, wrapServerApi } from "@/lib/utils";
+import { DishResType } from "@/schemaValidations/dish.schema";
+import Image from "next/image";
+import React from "react";
+
+export default async function DishDetail({
+  dish,
+}: {
+  dish: DishResType["data"] | undefined;
+}) {
+  if (!dish)
+    return (
+      <div>
+        <h1 className="text-2xl lg:text-3xl">Món ăn không tồn tại</h1>
+      </div>
+    );
+
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl lg:text-3xl">{dish.name}</h1>
+      <div className="font-semibold">Gía: {formatCurrency(dish.price)}</div>
+      <Image
+        src={dish.image}
+        alt={dish.name}
+        width={700}
+        height={700}
+        quality={100}
+        className="object-cover  w-[500px] h-[500px] rounded-md"
+      />
+      <p>{dish.description}</p>
+    </div>
+  );
+}
